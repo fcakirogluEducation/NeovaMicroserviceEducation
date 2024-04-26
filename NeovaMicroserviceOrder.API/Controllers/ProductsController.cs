4a64f78d7bd4aacc7cd43ceb3eaaa4156a3f4aa2
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2024-04-26 12:07:02 +0300

    docker compose konusu tamamlandı.

---
+++ NeovaMicroserviceOrder.API/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NeovaMicroserviceOrder.API.Models;
+
+namespace NeovaMicroserviceOrder.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(context.Products.ToList());
+        }
+    }
+}
